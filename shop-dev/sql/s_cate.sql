4cd6f65aac2e1d8c91c857fc9974f03a301a159a
yuanming <yuanming@esrgear.com>
2022-04-19 17:50:26 +0800

    shop-dev

--- shop-dev/sql/s_cate.sql
+++ shop-dev/sql/s_cate.sql
@@ -12,3 +12,11 @@ create table v_cate(
 	primary key(id)
 )ENGINE=INNODB default charset=utf8mb4 comment "product cate";
 
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (1, 'aa', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (2, 'bb', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (3, 'cc', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (4, 'bbb', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (5, 'bbb2', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (6, 'ccc', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (7, 'ccc2', 0, '2022-04-19 15:40:35', 0, NULL);
+INSERT INTO `shop`.`v_cate`(`id`, `cate_name`, `added_by`, `added_date`, `updated_by`, `updated_date`) VALUES (8, 'cccc', 0, '2022-04-19 15:40:35', 0, NULL);
--- shop-dev/sql/s_cate_rela.sql
+++ shop-dev/sql/s_cate_rela.sql
@@ -9,3 +9,11 @@ create table v_cate_rela(
 	unique key(cate_id, parent_id)
 )ENGINE=INNODB default charset=utf8mb4 comment "cate relation";
 
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (1, 1, 0);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (2, 2, 0);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (3, 3, 0);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (4, 4, 2);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (5, 5, 2);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (6, 6, 3);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (7, 7, 3);
+INSERT INTO `shop`.`v_cate_rela`(`id`, `cate_id`, `parent_id`) VALUES (8, 8, 6);
