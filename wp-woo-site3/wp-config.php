3a6c4c8014783f0511fbe4188ffcef387022d2e8
yuanming <yuanming@esrgear.com>
2022-06-01 18:23:37 +0800

    woo-site3

--- wp-woo-site3/wp-config.php
+++ wp-woo-site3/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'woo2' );
+define( 'DB_NAME', 'woo3' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
