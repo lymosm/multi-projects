b75ef5d00fdb8c52309d06827284b5d831321fc5
lymosmin <lymosmin@gmail.com>
2022-06-03 11:59:59 +0800

    woo2

--- wp-woo-site2/wp-config.php
+++ wp-woo-site2/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp1' );
+define( 'DB_NAME', 'woo2' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
